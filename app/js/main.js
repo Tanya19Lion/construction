5b702856c09a1d2e7a5e3b758ab998599c7d4b1d
Tanya19Lion <tanyatriteknitskaya@gmail.com>
2020-04-12 13:27:05 +0300

    added header and about sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,13 @@
 $(function(){
- 
+    
+    $(".header__main-inner").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false
+    });
 
 });
 
