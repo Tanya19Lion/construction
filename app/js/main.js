55ee72e6ab93cfdb002d8b119cf49c992404f9c1
Tanya19Lion <tanyatriteknitskaya@gmail.com>
2020-04-13 12:00:42 +0300

    added services section

--- app/js/main.js
+++ app/js/main.js
@@ -12,10 +12,17 @@ $(function(){
     $(".portfolio__inner").slick({
         slidesToShow: 4,
         slidesToScroll: 1,
-        arrows: false,
-        dots: true
-
+        prevArrow: '<button type="button" class="slider-btn slick-prev"></button>',
+        nextArrow: '<button type="button" class="slider-btn slick-next"></button>'
     });
 
+    $('.services__inner .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+            $('.services__inner').find('.tab-item').removeClass('active-tab').hide();
+            $('.services__inner .tabs').find('.tab').removeClass('active');
+            $(this).addClass('active');
+            $('#'+id).addClass('active-tab').fadeIn();
+            return false;
+        });
 });
 
